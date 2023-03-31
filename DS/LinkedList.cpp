3eaf3a49bd1d2a207abcc3afbd4aa9d18620c5aa
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2023-03-31 11:27:42 +0530

    LinkedList.cpp

---
+++ DS/LinkedList.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+void createNode(struct node **root, int *arr, int n){
+    cout<<"\n--addr root"<<root;
+    struct node *last= NULL;
+    for (int i=0;i<n;++i) {
+        struct node *t = new node();
+        t->data=arr[i];
+        t->next =NULL;
+        if (*root == NULL) {
+            *root =t;
+            last=t;
+        }
+        else {
+            last->next=t;
+            last=t;
+        }
+    }//for
+    cout<<"\n root"<<root;
+}
+
+
+void Display(struct node **root) {
+    cout<<"Dispaly root"<<root;
+    struct node *temp =*root;
+    while(temp !=NULL)
+    {
+        cout<<" "<<temp->data;
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    cout<<"Hello World";
+    int arr[] = {4,5,10,21,40};
+    int size = (sizeof(arr)/arr[1]);
+    cout<<"arr size="<<size;
+    
+    struct node *root =NULL;
+    cout<<"addr root"<<&root;
+    createNode(&root,arr,size);
+    cout<<"**root**"<<&root<<"\n";
+    Display(&root);
+    return 0;
+}

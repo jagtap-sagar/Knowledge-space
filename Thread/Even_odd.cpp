a30e2e75832f527d63262d531468c41be93ab540
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2024-04-04 12:52:26 +0530

    Create Even_odd.cpp

---
+++ Thread/Even_odd.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<mutex>
+#include<pthread.h>
+#include<cstdlib>
+int count=0;
+using namespace std;
+mutex m;
+void* printEven(void *a)
+{
+   while(1)
+   {
+       m.lock();
+       if(count%2==0)
+       {
+          cout<<" I am Even"<<count<<endl;
+          count++;
+       }
+       if(count==100)
+           break;
+       m.unlock();
+   }
+}
+void* printOdd(void *b)
+{
+    while(1)
+    {
+       m.lock();
+       if(count%2!=0)
+       {
+           cout<<"I am odd"<<count<<endl;
+           count++;
+       }
+       if(count>100)
+          break;
+       m.unlock();
+    }
+ }
+ int main()
+ {
+     int *ptr = new int();
+     pthread_t thread1, thread2;
+     pthread_attr_t attr;
+     pthread_attr_init(&attr);
+     pthread_create(&thread1,&attr,&printEven,NULL);
+     pthread_create(&thread2,&attr,&printOdd, NULL);
+     pthread_join(thread1,&ptr);
+     pthread_join(thread2,&ptr);
+     delete ptr;
+ }

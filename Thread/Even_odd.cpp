0137d4ff6fc8df0ea53fc7f21db152692b1d970f
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2024-04-04 15:32:10 +0530

    Update Even_odd.cpp

--- Thread/Even_odd.cpp
+++ Thread/Even_odd.cpp
@@ -47,3 +47,85 @@ void* printOdd(void *b)
      pthread_join(thread2,(void **)&ptr);
      delete ptr;
  }
+--------------more optimized --------------------------------------
+
+   #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include<iostream>
+using namespace std;
+ 
+pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  condition_var   = PTHREAD_COND_INITIALIZER;
+ 
+void *functionCount1(void *temp);
+void *functionCount2(void *temp);
+int  count = 1;
+#define COUNT_DONE  10
+#define COUNT_HALT1  3
+#define COUNT_HALT2  6
+ 
+main()
+{
+
+ pthread_t thread1, thread2;
+ pthread_create( &thread1, NULL, &functionCount1, NULL);
+
+ pthread_create( &thread2, NULL, &functionCount2, NULL);
+
+ pthread_join( thread1, NULL);
+
+ pthread_join( thread2, NULL);
+ printf("Final count: %d\n",count);
+   exit(0);
+}
+ 
+// Write numbers 1-3 and 8-10 as permitted by functionCount2()
+ 
+void *functionCount1(void *temp)
+{
+ for(;;)
+ {
+    // Lock mutex and then wait for signal to relase mutex
+
+    pthread_mutex_lock( &count_mutex );
+    // Wait while functionCount2() operates on count
+    // mutex unlocked if condition varialbe in functionCount2() signaled.
+    pthread_cond_wait( &condition_var, &count_mutex );
+    //count++;
+    printf("Counter value functionCount1: %d\n",count);
+    count++;
+    pthread_mutex_unlock( &count_mutex );
+    if(count >= COUNT_DONE) return(NULL);
+  }
+
+}
+ 
+// Write numbers 4-7
+ //odd
+void *functionCount2(void *temp)
+{
+  for(;;)
+  {
+     pthread_mutex_lock( &count_mutex );
+     if( count %2 ==0 )
+     {
+        // Condition of if statement has been met.
+        // Signal to free waiting thread by freeing the mutex.
+        // Note: functionCount1() is now permitted to modify "count".
+        //cout<<"-------------"<<count;
+        pthread_cond_signal( &condition_var );
+     }
+
+     else
+     {
+        printf("Counter value functionCount=>2: %d\n",count);
+        count++;
+     }
+     pthread_mutex_unlock( &count_mutex );
+     if(count >= COUNT_DONE) return(NULL);
+  }
+   exit(0);
+}
+
+ 

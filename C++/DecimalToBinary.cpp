65fcaf6b766733e74fa0ed2dedc3acd9f00e6f84
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2025-04-22 13:39:34 +0530

    Create DecimalToBinary.cpp
    Simple mod operatiopm

---
+++ C++/DecimalToBinary.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int DecimalToBinary(int num) {
+    string binary;
+    for (;num >0 ;num /=2) {
+      /*  if (num %2 ==0)
+            binary += '0';
+        else  //num%2 ==1 
+            binary += '1';
+            */
+        binary += ((num%2))?'1':'0';    
+    }
+    
+    cout<<"=="<<binary<<"\n";
+    //return atoi(newNum);
+    std::reverse(binary.begin(), binary.end());
+    return stoi(binary);
+}
+
+int main()
+{
+    int input =5;
+    std::cout<<"DecimalToBinary"<<DecimalToBinary(input);
+
+    return 0;
+}

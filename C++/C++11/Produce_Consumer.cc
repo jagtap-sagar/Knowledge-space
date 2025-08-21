86fc701003bbee96eaf102a12254a0fcd41499cf
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2025-08-21 17:38:10 +0530

    Create Produce_Consumer.cc

---
+++ C++/C++11/Produce_Consumer.cc
@@ -0,0 +1,55 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<thread>
+#include<queue>
+#include<condition_variable>
+using namespace std;
+//C++14 tread producer consumer problem
+
+mutex mt;
+condition_variable cv;
+queue<int> q;
+bool finished = false;
+
+const int N=5;
+void producer(){
+ for (int i=0;i<N;++i){
+     {
+        lock_guard<mutex> lock(mt);
+        q.push(i+1);
+        cout<<" produced :"<<i+1;
+     }
+      cv.notify_one();
+         //sleep
+ } //for   
+    
+     {
+        lock_guard<mutex> lock(mt);
+        finished =true;
+     }
+      cout<<"\n";
+    cv.notify_one();
+}
+
+void consumer(){
+   
+    while(true){
+      
+        unique_lock<mutex> lock(mt);
+        cv.wait(lock,[]{ return !q.empty() ||finished;  });
+        if(q.empty() && finished) {break;}
+        if(!q.empty()){
+            cout<<" Consumed :"<<q.front();
+            q.pop();
+        }
+        lock.unlock();
+    }
+}
+
+int main() {
+    thread prod(producer);
+    thread consu(consumer);
+    prod.join();
+    consu.join();
+    return 0;
+}

c71b758db234b379ca76bc0bc7851a796fdade83
jagtap-sagar <125564352+jagtap-sagar@users.noreply.github.com>
2025-12-05 10:51:53 +0530

    Update TwoLLIntersect.cc

--- C++/DS/TwoLLIntersect.cc
+++ C++/DS/TwoLLIntersect.cc
@@ -2,5 +2,6 @@ Approach
 Count Lengths: Calculate the lengths of both linked lists.
 Align Lists: Advance the pointer of the longer list by the difference in lengths to ensure both pointers traverse the 
 same remaining distance.
-Traverse Together: Move both pointers one step at a time until they meet at a node (intersection point) or reach the end (nullptr, no intersection).
+Traverse Together: Move both pointers one step at a time until they meet at a node (intersection point) or reach the end (nullptr,
+no intersection).
 Return Result: If they meet at a non-null node, return that node as the intersection point; otherwise, return nullptr.
